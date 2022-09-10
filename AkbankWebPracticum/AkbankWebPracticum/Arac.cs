b109194380bf2ff142a661ee50aa1f1f7998fe5f
Onur Atalık <72446425+WarLock54@users.noreply.github.com>
2022-09-10 22:39:23 +0300

    Add files via upload

---
+++ AkbankWebPracticum/AkbankWebPracticum/Arac.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkbankWebPracticum
+{
+    abstract class Arac
+    {
+        Arac arac;
+        public int HGSNO { get; set; }
+        public string İsim { get; set; }
+        public string Soyisim { get; set; }
+        public decimal Bakiye { get; set; }
+        public DateTime Zaman { get; set; }
+        public Arac(int _HGSNO, string _İsim, string _Soyisim)
+        {
+            HGSNO = _HGSNO;
+            İsim = _İsim;
+            Soyisim = _Soyisim;
+            Zaman = DateTime.Now;
+         
+        }
+        public abstract string GecisParaUcreti();
+        public override string ToString()
+        {
+           // DateTime.Parse(Zaman);
+            StringBuilder str = new StringBuilder();
+            str.Append($"{İsim} {Soyisim} {HGSNO} lu kisi  zaman: { Zaman} ");
+            return str.ToString();
+        }
+
+
+
+    }
+    class Otomobil : Arac
+    {
+        private List<Otomobil> Otomobils;
+        public Otomobil(int _HGSNO, string _İsim, string _Soyisim) : base(_HGSNO, _İsim, _Soyisim)
+        {
+           
+        }
+        public override string GecisParaUcreti()
+        {
+                
+                this.Bakiye=100;
+            return "Otomobil vasitasiyla gittiginiz icin" + this.Bakiye;
+        }
+
+      
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(base.ToString());
+            str.Append($" ucret : {this.GecisParaUcreti()}");
+       return str.ToString();
+        }
+
+    }
+    class Minubus : Arac
+    {
+        public Minubus(int _HGSNO, string _İsim, string _Soyisim) : base(_HGSNO, _İsim, _Soyisim)
+        {
+
+        }
+        public override string GecisParaUcreti()
+        {
+            this.Bakiye = 250;
+            return "Minubus vasitasiyla gittiginiz icin" + this.Bakiye;
+        }
+
+       
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(base.ToString());
+            str.Append($" ucret : {this.GecisParaUcreti()}");
+            return str.ToString();
+        }
+
+    }
+    class Otobus : Arac
+    {
+        public Otobus(int _HGSNO, string _İsim, string _Soyisim) : base(_HGSNO, _İsim, _Soyisim)
+        {
+
+        }
+        public override string GecisParaUcreti()
+        {
+            this.Bakiye = 500;
+            return "Otobus vasitasiyla gittiginiz icin" + this.Bakiye;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append(base.ToString());
+            str.Append($" ucret : {this.GecisParaUcreti()}");
+            return str.ToString();
+        }
+
+    }
+
+}
---
+++ AkbankWebPracticum/AkbankWebPracticum/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AkbankWebPracticum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+             Otomobil otomobil = new Otomobil(1,"ONUR","ATALİK");
+            MessageBox.Show(otomobil.ToString());
+            Minubus minubus = new Minubus(2, "elif", "Simsek");
+            Otobus otobus = new Otobus(3, "HAKAN", "KUPLU");
+        }
+    }
+}
