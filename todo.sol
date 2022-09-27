a134eebad1f5ae2e949eb59f8d66532a58d6fd3d
Onur Atalık <72446425+WarLock54@users.noreply.github.com>
2022-09-27 15:24:43 +0300

    Add files via upload

--- todo.sol
+++ todo.sol
@@ -6,7 +6,11 @@ contract toDoList{
 string text;
 bool completed;
     }
+    // todo listesi oluşturuyoruz...
     toDo[] public todos;
+    /*
+        todos array ine  yeni bilgiler eklenmesi için oluşturulan fonksiyon...
+     */
     function create(string calldata _text)public {
             todos.push(toDo(_text,false));
             todos.push(toDO(text:_text,completed:false));
@@ -14,15 +18,23 @@ bool completed;
             todo.text=_text;
             todos.push(todo);
     }
-
+/* 
+    todos array indeki bilgilere erişmek için oluşturalan fonksiyon
+ */
     function get(uint _index)public view returns(string memory text,bool completed){
 toDO storage td=todos[_index];
 return (td.text,td.completed);
     }
+    /*
+        todos arrayindeki indexin bilgilerini değiştirmek için oluşturuldu...
+     */
     function update(string memory _text,uint _index)public{
         toDO storage td=todos[_index];
         td.text=_text;
     }
+    /*
+    tamamlanıp tamamlanmama kontorulu...
+     */
     function toggleCompleted(uint _index) public{
         toDO storage td=todos[_index];
         td.completed=!td.completed;
